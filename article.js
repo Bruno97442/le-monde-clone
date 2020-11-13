6b6291b950760a2f32e1d5a0b920ded5e66aa8cf
Bruno <benard.bruno97@gmail.com>
2020-11-13 11:58:32 +0100

    fin de l'article générale, définition de l'article en headlines en cour

--- article.js
+++ article.js
@@ -1,38 +1,41 @@
 const articlesmoteur = (nbr, articleData) => {
     let article = (nbr, articleData) => `<article class="${articleData} article${nbr}">
-    <header>
-        <a href="">
+    <a href="">
+            <header>
 
-            <h${articleData == "une" ? 1 : 2}>
-            
-                <span class="puce">M</span>
-                <span>Titre sa mère dolor sit amet consectetur. Maxime odit, cumque atque recusandae!</span>
-            </h${articleData == "une" ? 1 : 2}>
-                <picture class="article-picture">
-                    <img src="img/wonderwomanCoffee.png" alt="C'est de l'article">
-                </picture>
+                <h${articleData == "une" ? 1 : 2}>
+                
+                    <span class="puce">M</span>
+                    <span>Titre sa mère dolor sit amet consectetur. Maxime odit, cumque atque recusandae!</span>
+                </h${articleData == "une" ? 1 : 2}>
+                
+                
+            </header>
+            <main>
+                <section class="article-media">
+                    <picture class="article-picture">
+                        <img src="img/wonderwomanCoffee.png" alt="C'est de l'article">
+                    </picture>
+                </section>
                 <p>
-                    Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum provident numquam dolor eos unde repellendus soluta? 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
+                    provident numquam dolor eos unde repellendus soluta?
                 </p>
-            </a>
-        </header>
-        <main>
-            <picture class="article-picture">
-                <img src="" alt="">
-            </picture>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
-                provident numquam dolor eos unde repellendus soluta?
-            </p>
-
-        </main>
+            </main>
+        </a>
         <footer>
             <ul class="sameway-articles">
                 <li>
-                    <article></article>
+                        <a>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
+                            provident numquam dolor eos unde repellendus soluta?
+                        </a>
                 </li>
                 <li>
-                    <article></article>
+                        <a>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
+                            provident numquam dolor eos unde repellendus soluta?
+                        </a>
                 </li>
             </ul>
         </footer>
@@ -47,7 +50,6 @@ const articlesmoteur = (nbr, articleData) => {
 }
 
 document.querySelectorAll('article[data-c]').forEach(ele => {
-    console.log(ele.dataset.c)
     ele.insertAdjacentHTML("afterend", articlesmoteur(ele.dataset.n, ele.dataset.c))
     ele.remove()
 }

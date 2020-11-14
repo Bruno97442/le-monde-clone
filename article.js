bafcd798b0371567a3e67ca7738448a28981a4a2
bruno <benard.bruno97@gmail.com>
2020-11-14 20:47:45 +0100

    finition du style de l'article featured et mise en place du grid le la homepage sur écran > 1024px

--- article.js
+++ article.js
@@ -1,45 +1,50 @@
 const articlesmoteur = (nbr, articleData) => {
-    let article = (nbr, articleData) => `<article class="${articleData} article${nbr}">
-    <a href="">
-            <header>
-
-                <h${articleData == "une" ? 1 : 2}>
-                
-                    <span class="puce">M</span>
-                    <span>Titre sa mère dolor sit amet consectetur. Maxime odit, cumque atque recusandae!</span>
-                </h${articleData == "une" ? 1 : 2}>
-                
-                
-            </header>
-            <main>
-                <section class="article-media">
-                    <picture class="article-picture">
-                        <img src="img/wonderwomanCoffee.png" alt="C'est de l'article">
-                    </picture>
-                </section>
-                <p>
+    let article = (nbr, articleData) => 
+        `<article class="${articleData} article${nbr}">
+            <a href="">
+                <header>
+                    <h${articleData == "une" ? 1 : 2}>
+                        <span class="puce">${articleData} M</span>
+                        <span>Titre sa mère dolor sit amet consectetur. Maxime odit, cumque atque recusandae!</span>
+                    </h${articleData == "une" ? 1 : 2}>
+             
+                </header>
+                <main>
+                    <section class="article-media">
+                        <picture class="article-picture">
+                            <img src="img/wonderwomanCoffee.png" alt="C'est de l'article">
+                        </picture>
+                    </section>
+                    ${articleData != "featured" ? 
+                    `<p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
+                        provident numquam dolor eos unde repellendus soluta?
+                    </p>` : ""}
+                </main>
+                ${articleData == "featured" ? 
+                `<p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
                     provident numquam dolor eos unde repellendus soluta?
-                </p>
-            </main>
-        </a>
-        <footer>
-            <ul class="sameway-articles">
-                <li>
-                        <a>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
-                            provident numquam dolor eos unde repellendus soluta?
-                        </a>
-                </li>
-                <li>
-                        <a>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
-                            provident numquam dolor eos unde repellendus soluta?
-                        </a>
-                </li>
-            </ul>
-        </footer>
-</article>`, articles =""
+                </p>` : ""}
+            </a>
+                ${articleData !== "une" ? "" : `
+                <footer>
+                    <ul class="sameway-articles">
+                        <li>
+                                <a>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
+                                    provident numquam dolor eos unde repellendus soluta?
+                                </a>
+                        </li>
+                        <li>
+                                <a>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo natus rerum
+                                    provident numquam dolor eos unde repellendus soluta?
+                                </a>
+                        </li>
+                    </ul>
+                </footer> `}
+        </article>`, articles =""
 
     for (let i = 0; i < nbr; i++) {
         articles += article(i, articleData);
